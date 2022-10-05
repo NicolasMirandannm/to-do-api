fee7817bfcd3d6da0192697884ff9ec5e2d79674
Nicolas Leonardo Miranda Lima <nicolasleonardonnm@gmail.com>
2022-10-04 20:57:44 -0400

    express, and routes created

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("teste");
+import express from "express";
+import routes from "./routes/ToDoRoutes";
+
+const app = express();
+const port = 3000;
+
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`Server listening in http://localhost:${port}`);
+});
