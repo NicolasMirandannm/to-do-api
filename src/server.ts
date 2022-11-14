c79b3f6241e86982c369881c0d35283bca69cb96
NicolasMirandannm <nicolasleonardonnm@gmail.com>
2022-11-14 18:34:17 -0400

    ativação do cors e ajuste no findAll de tarefas

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,15 @@ import express from "express";
 import { listRoutes } from "./routes/ListRoutes";
 import { taskAndListRoutes } from "./routes/TaskAndListRoutes";
 import { taskRoutes } from "./routes/TaskRoutes";
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
+const options: cors.CorsOptions = {
+  origin: '*'
+}
 
+app.use(cors(options));
 app.use(express.json());
 app.use(taskRoutes, listRoutes, taskAndListRoutes);
 
--- src/services/tasks/CRUD/ListAllTasks.ts
+++ src/services/tasks/CRUD/ListAllTasks.ts
@@ -3,7 +3,11 @@ import prisma from "../../../database/PrismaService";
 export class ListAllTasks {
     
     public async exec() {
-        const allTasks = await prisma.task.findMany({})
+        const allTasks = await prisma.task.findMany({
+            include: {
+                list:true
+            }
+        })
         return allTasks;
     }
 }
